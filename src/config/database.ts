e676cfff03913c754f49e162899f40a135548682
Vitor Xavier <vitor.xavier.dev@gmail.com>
2025-01-28 22:39:23 -0300

    chore: Add SSL

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,19 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Necessário para alguns provedores como Heroku/Render
+  }
+});
+
+// Teste de conexão
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Erro ao conectar ao banco:', err.stack);
+  }
+  console.log('Conectado ao banco com sucesso!');
+  release();
 });
 
 export default pool; 
