ac43f365f2f1e5ad01377c02037a527022cd68e9
Vitor Xavier <vitor.xavier.dev@gmail.com>
2025-01-28 22:36:39 -0300

    chore: Configurando database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_u2A8jOhEfLrF@ep-old-glitter-a86pwjol-pooler.eastus2.azure.neon.tech/neondb?sslmode=require'
+  connectionString: process.env.DATABASE_URL
 });
 
 export default pool; 
